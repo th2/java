69476f9802a92daaad7136f98f1fe24a45952140
th2 <github2@c8.io>
2014-05-15 13:09:34 +0200

    added String getPage(URL url)

--- Araneae/src/Crawler.java
+++ Araneae/src/Crawler.java
@@ -1,16 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.LinkedList;
 
 
 public class Crawler {
-	LinkedList<String> queue = new LinkedList<String>();
-	HashMap<String, Integer> found = new HashMap<String, Integer>();
+	LinkedList<URL> queue = new LinkedList<URL>();
+	HashMap<URL, Integer> found = new HashMap<URL, Integer>();
 	
-	public void addToQueue(String url){
+	public void addToQueue(URL url){
 		queue.add(url);
+	}
+	
+	public String getPage(URL url){
+		try {
+			String line;
+			StringBuilder page = new StringBuilder();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+			while ((line = reader.readLine()) != null) {
+				page.append(line);
+	        }
+			return page.toString();			
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "";
+		}
 	}
 
 	public void crawl() {
-		
+		//TODO: add randomization 
+		//TODO: add exception handling if queue is empty
+		String page = getPage(queue.removeFirst());
 	}
 }
--- Araneae/src/Main.java
+++ Araneae/src/Main.java
@@ -1,10 +1,17 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		Crawler crawler = new Crawler();
-		crawler.addToQueue("http://en.wikipedia.org/");
-		crawler.crawl();
+		try {
+			crawler.addToQueue(new URL("http://en.wikipedia.org/"));
+			crawler.crawl();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
