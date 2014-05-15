ae1acd0c0c53b8810c37b35ee419dcda326729fb
th2 <github2@c8.io>
2014-05-15 14:14:31 +0200

    replaced getPage(URL url) with jsoup equivalent

--- Araneae/src/Crawler.java
+++ Araneae/src/Crawler.java
@@ -1,9 +1,11 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.LinkedList;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 
 public class Crawler {
@@ -13,25 +15,15 @@ public class Crawler {
 	public void addToQueue(URL url){
 		queue.add(url);
 	}
-	
-	public String getPage(URL url){
-		try {
-			String line;
-			StringBuilder page = new StringBuilder();
-			BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-			while ((line = reader.readLine()) != null) {
-				page.append(line);
-	        }
-			return page.toString();			
-		} catch (IOException e) {
-			e.printStackTrace();
-			return "";
-		}
-	}
 
 	public void crawl() {
-		//TODO: add randomization 
-		//TODO: add exception handling if queue is empty
-		String page = getPage(queue.removeFirst());
+		try {
+			//TODO: add randomization 
+			//TODO: add exception handling if queue is empty
+			Document doc = Jsoup.connect(queue.removeFirst().getPath()).get();
+			Elements as = doc.select("a");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
