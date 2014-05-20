55614fda41e533e062ca7964b89fd1319773693d
th2 <github2@c8.io>
2014-05-20 16:45:28 +0200

    save pages functionality

--- Araneae/src/Crawler.java
+++ Araneae/src/Crawler.java
@@ -3,11 +3,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Random;
-
 import java.util.Map.Entry;
+
 
 //imports require jsoup-1.7.3.jar core library from http://jsoup.org/download
 import org.jsoup.Jsoup;
@@ -18,16 +19,21 @@ import org.jsoup.select.Elements;
 
 public class Crawler {
 	boolean deterministic;
+	static boolean savePages;
+	static ArrayList<String> saveLog = new ArrayList<String>();
+	int saveCounter = 0;
 	LinkedList<String> queue = new LinkedList<String>();
 	HashMap<String, Integer> found = new HashMap<String, Integer>();
 	HashMap<String, Integer> numlinks = new HashMap<String, Integer>();
 	
 	Crawler(){
 		deterministic = false;
+		savePages = false;
 	}
 	
-	Crawler(boolean deterministic){
+	Crawler(boolean deterministic, boolean savePages){
 		this.deterministic = deterministic;
+		this.savePages = savePages;
 	}
 	
 	public void addToQueue(String url){
@@ -63,6 +69,14 @@ public class Crawler {
 			Document doc = Jsoup.connect(newLink).get();
 			Elements links = doc.select("a[href]");
 			numlinks.put(newLink, links.size());
+			
+			if(savePages){
+				saveLog.add(newLink);
+				PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter((saveCounter++)+".html", true)));
+				out.println(doc.toString());
+				out.close();
+			}
+			
 			for(Element link : links){
 				//System.out.println(link.attr("abs:href"));
 				String url = cleanup(link.attr("abs:href"));
@@ -82,39 +96,47 @@ public class Crawler {
 	}
 	
 	public static void main(String[] args) {
-		if(args.length != 5)
+		if(args.length != 6)
 			System.out.println("parameters: "
 					+ "[START URL] "
 					+ "[NUMBER OF PAGES] "
 					+ "[DETERMINISTIC] "
-					+ "[SAVE RESULTS] "
+					+ "[SAVE PAGES] "
 					+ "[OUT FILE FOUND] "
 					+ "[OUT FILE NUMLINKS] "
 					+ System.lineSeparator()
 					+ "example: java -cp ../jsoup-1.7.3.jar:. Crawler http://en.wikipedia.org/ 1500 true false found.txt numlinks.txt");
 		else{
 			int numPages = Integer.parseInt(args[1]);
-			Crawler crawler = new Crawler(Boolean.parseBoolean(args[2]));
+			Crawler crawler = new Crawler(Boolean.parseBoolean(args[2]), Boolean.parseBoolean(args[3]));
 			crawler.addToQueue(args[0]);
 			for(int i = 0; i<numPages; i++){
 				crawler.crawl();
 				System.out.println("crawled "+i+" pages");
 			}
 
-			HashMap<String, Integer> sortedFound = (HashMap<String, Integer>) SortMap.sortByValue(crawler.found);
-			HashMap<String, Integer> sortedNumlinks = (HashMap<String, Integer>) SortMap.sortByValue(crawler.numlinks);
 			try {
+				HashMap<String, Integer> sortedFound = (HashMap<String, Integer>) SortMap.sortByValue(crawler.found);
 				PrintWriter outFound = new PrintWriter(new BufferedWriter(new FileWriter(args[4], true)));
 				for(Entry<String, Integer> entry : sortedFound.entrySet()) {
 					outFound.println(entry.getValue()+" "+entry.getKey());
 				}
 				outFound.close();
-			    
+
+				HashMap<String, Integer> sortedNumlinks = (HashMap<String, Integer>) SortMap.sortByValue(crawler.numlinks);
 				PrintWriter outNumlinks = new PrintWriter(new BufferedWriter(new FileWriter(args[5], true)));
 				for(Entry<String, Integer> entry : sortedNumlinks.entrySet()) {
 					outNumlinks.println(entry.getValue()+" "+entry.getKey());
 				}
 				outNumlinks.close();
+				
+				if(savePages) {
+					PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("saveLog.txt", true)));
+					for(String entry : saveLog) {
+						out.println(entry);
+					}
+					out.close();
+				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
